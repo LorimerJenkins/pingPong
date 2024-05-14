680caf8032707d4886f461114c238cd9f75cc37a
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-05-14 19:15:17 +0100

    chore: delete cron check

--- pingPong.lua
+++ pingPong.lua
@@ -7,13 +7,3 @@ Handlers.add(
 )
 
 
-Handlers.add(
-  "CronTick", -- handler name
-  Handlers.utils.hasMatchingTag("Action", "Cron"), -- handler pattern to identify cron message
-  function () -- handler task to execute on cron message
-    -- do something
-  end
-)
-
-
-
